1f06472fd885df204a512f00354c08c6d097e613
soyunlee882 <soyunlee882@gmail.com>
2017-06-13 20:33:41 +0900

    #3Event Handling 1(init Button)

--- app/src/main/java/syl/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/syl/kr/hs/emirim/dbex/MainActivity.java
@@ -5,20 +5,48 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editName, editCount,editResultName, editResultCount;
+    Button butInit, butInsert, butSelect;
+    MyDBHlpe myHelper;
+    SQLiteDatabase sqldb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName=(EditText)findViewById(R.id.edit_group_name);
+        editCount=(EditText)findViewById(R.id.edit_group_count);
+        editResultName=(EditText)findViewById(R.id.edit_result_name);
+        editResultCount=(EditText)findViewById(R.id.edit_group_count);
+        butInit=(Button)findViewById(R.id.but_init);
+        butInsert=(Button)findViewById(R.id.but_insert);
+        butSelect=(Button)findViewById(R.id.but_selecet);
+
+        //DB 생성
+        myHelper=new MyDBHlpe(this);
+        //기존의 테이블이 존재하면 삭제하고 테이블을 새로 생성한다.
+        butInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               sqldb=myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqldb,1,2);
+                sqldb.close();
+            }
+        });
     }
+
     class MyDBHlpe extends SQLiteOpenHelper{
 
         public MyDBHlpe(Context context) {
             //idolDb라 이름의 테이터 베이스가 생성된다.
             // public MyDBHlpe(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-            super(context, "idolDB",null,1);
+            super(context, "idolDB", null, 1);
+        }
 
         //idolTavble 라는 이름의 테이블 생성
             //
@@ -36,11 +64,5 @@ public class MainActivity extends AppCompatActivity {
                 onCreate(sqLiteDatabase);
         }
     }
-
-
-
-
-
-
 
 }
