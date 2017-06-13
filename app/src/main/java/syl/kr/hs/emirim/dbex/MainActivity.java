e42351dd155417bf2ceb36b5049d9523e9bf2b32
soyunlee882 <soyunlee882@gmail.com>
2017-06-13 20:46:40 +0900

    #4Event Handling 2(input Button)

--- app/src/main/java/syl/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/syl/kr/hs/emirim/dbex/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText editName, editCount,editResultName, editResultCount;
@@ -36,6 +37,16 @@ public class MainActivity extends AppCompatActivity {
                sqldb=myHelper.getWritableDatabase();
                 myHelper.onUpgrade(sqldb,1,2);
                 sqldb.close();
+            }
+        });
+        butInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                sqldb=myHelper.getWritableDatabase();
+                String sql="insert into idolTable values('"+editName.getText()+"',"+editCount.getText()+")";
+                sqldb.execSQL(sql);
+                sqldb.close();
+                Toast.makeText(MainActivity.this,"저장되었습니다",Toast.LENGTH_LONG).show();
             }
         });
     }
